8b1c245c776b878b97a6e68870bbff51f7dbd1c1
Sunil Kumar <sunil.divyam@gmail.com>
2024-03-04 18:28:32 +0530

    Firebase config Updates

--- src/app/constants/firebase.config.development.ts
+++ src/app/constants/firebase.config.development.ts
@@ -3,15 +3,16 @@ import {
   FireStorageConfig,
   FireStoreConfig,
   FirebaseConfig,
-} from '@annuadvent/ngx-tools/fire-common';
-import { appConfig } from './app.config.development';
-import { FIREBASE_AUTH_SIGNIN_METHODS } from '@annuadvent/ngx-tools/fire-auth';
+} from "@annuadvent/ngx-tools/fire-common";
+import { appConfig } from "./app.config.development";
+import { FIREBASE_AUTH_SIGNIN_METHODS } from "@annuadvent/ngx-tools/fire-auth";
 
 const ui = {
-  signInFlow: 'popup', // redirect
+  signInFlow: "redirect", // redirect/popup
   siteName: appConfig.name,
   tosUrl: appConfig.tNcUrl, // Terms of service page url
   privacyPolicyUrl: appConfig.privacyPolicyUrl, // Privacy policy url
+  signInSuccessUrl: "https:localhost:4200/stories",
   // signInSuccessUrl: '/components/auth/login-status',  // User is redirected to this url after successful login.
 
   // Callback methods, on login events, like success, failure etc. and
@@ -33,12 +34,12 @@ const ui = {
     {
       provider: FIREBASE_AUTH_SIGNIN_METHODS.PHONE,
       recaptchaParameters: {
-        type: 'image', // or 'audio'
-        size: 'normal', // or 'invisible' or 'compact'
-        badge: 'bottomleft', //' bottomright' or 'inline' applies to invisible.
+        type: "image", // or 'audio'
+        size: "normal", // or 'invisible' or 'compact'
+        badge: "bottomleft", //' bottomright' or 'inline' applies to invisible.
       },
-      defaultCountry: 'IN',
-      loginHint: '+91-1234567890',
+      defaultCountry: "IN",
+      loginHint: "+91-1234567890",
     },
     // Sign in with Google
     {
@@ -61,30 +62,32 @@ const ui = {
 };
 
 const app: FireAppConfig = {
-  projectId: 'annu-business',
-  appId: '1:140977750488:web:870df7cc0848f6274ab2ad',
-  storageBucket: 'annu-business.appspot.com',
-  locationId: 'us-central',
-  apiKey: '',
-  authDomain: 'annu-business.firebaseapp.com',
-  messagingSenderId: '140977750488',
-  measurementId: 'G-36J6R2BDWD',
+  projectId: "annu-business",
+  appId: "1:140977750488:web:870df7cc0848f6274ab2ad",
+  storageBucket: "annu-business.appspot.com",
+  locationId: "us-central",
+  apiKey: "",
+  authDomain: "annu-business.firebaseapp.com",
+  messagingSenderId: "140977750488",
+  measurementId: "G-36J6R2BDWD",
 };
 
 const storage: FireStorageConfig = {
-  baseStorageUrl: 'annu-business/articles',
-  fireStorageBaseApiUrl: 'https://firebasestorage.googleapis.com/v0/b/annu-business.appspot.com/o',
+  baseStorageUrl: "annu-business/articles",
+  fireStorageBaseApiUrl:
+    "https://firebasestorage.googleapis.com/v0/b/annu-business.appspot.com/o",
   imageDimensions: {
     maxKBs: 1024,
     maxWidth: 900,
     maxHeight: 450,
     minWidth: 100,
     minHeight: 100,
-  }
+  },
 };
 
 const store: FireStoreConfig = {
-  firestoreBaseApiUrl: 'https://firestore.googleapis.com/v1/projects/annu-business/databases/(default)/documents',
+  firestoreBaseApiUrl:
+    "https://firestore.googleapis.com/v1/projects/annu-business/databases/(default)/documents",
 };
 
 export const firebaseConfig: FirebaseConfig = {
@@ -92,4 +95,4 @@ export const firebaseConfig: FirebaseConfig = {
   ui,
   store,
   storage,
-}
+};
